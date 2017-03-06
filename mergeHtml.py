af2044f70c07e978b06bfa214d8e632171c2d1b3
yanzhongsu <syz250@foxmail.com>
2017-03-06 21:50:47 +0000

    fix small bug

--- mergeHtml.py
+++ mergeHtml.py
@@ -2,7 +2,7 @@ import os
 import json
 
 def read(fdir):
-	
+
 	with open(fdir) as json_data:
 		json_data = json.load(json_data)
 	return json_data
@@ -23,7 +23,7 @@ def main():
 	dir_cur = os.getcwd() + '/gap-html'
 
 	json_data = read(dir_cur + '/text.json')
-    file_name = dir_cur + '/doc.json'
+	file_name = dir_cur + '/doc.json'
 
 	for doc in json_data:
 
@@ -31,7 +31,7 @@ def main():
 		data = {'dir': doc['dir'], 'text':content}
 
 		with open(file_name, 'a') as outfile:
-        	json.dump(data, outfile)
+			json.dump(data, outfile)
 
 if __name__ == '__main__':
 	main()
