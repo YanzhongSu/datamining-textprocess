0565ef7a5c55e713ffb73d12892a54094aeb1100
yanzhongsu <syz250@foxmail.com>
2017-03-07 20:16:34 +0000

    add code to feacture_extraction.py

---
+++ feature_extraction.py
@@ -0,0 +1,32 @@
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+
+def read(fdir):
+	with open(fdir) as json_data:
+		json_data = json.load(json_data)
+
+	corpus = []
+	index = []
+	for fitem in json_data:
+		corpus.append(fitem['text'])
+		index.append(fitem['dir'])
+
+	return corpus, index
+
+def main():
+
+	dir_cur = os.getcwd() + '/gap-html'
+
+	corpus, index = read(dir_cur + '/doc.json')
+
+	vectorizer = CountVectorizer()
+	x = vectorizer.fit_transform(corpus)
+
+	vector = x.toarray()
+	print "vector element number, should be 24:", len(vector)
+
+	for i in xrange(len(vector)):
+		print "the vector length of file", index[i], "is:", len(vector[i])
+
+if __name__ == '__main__':
+	main()
