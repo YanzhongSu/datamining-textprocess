7810a97d668994e396e49e05b33b4cc247d9ad9c
yanzhongsu <syz250@foxmail.com>
2017-03-11 20:24:26 +0000

    comment print part in K-Mean

--- feature_extraction.py
+++ feature_extraction.py
@@ -87,7 +87,8 @@ def main():
 	for i in range(k):
 		print "Cluster :", i, "has", len(cat[i]), "documents"
 		for ind in order_centroids[i, :10]:
-			print terms[ind],
+			# print terms[ind],
+			pass
 		print()
 
 		for item in cat[i]:
