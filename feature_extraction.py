a541f6c97fbecd9c5f4b876fa5b22cab14671c87
yanzhongsu <syz250@foxmail.com>
2017-03-11 20:51:43 +0000

    print top terms in KMean

--- feature_extraction.py
+++ feature_extraction.py
@@ -118,7 +118,7 @@ def main():
 		for i in range(k):
 			print "Cluster :", i, "has", len(cat[i]), "documents"
 			for ind in order_centroids[i, :10]:
-				# print terms[ind],
+				print terms[ind],
 				pass
 			print
 
